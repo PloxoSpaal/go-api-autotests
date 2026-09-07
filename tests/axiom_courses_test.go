f677b281b2b506a6f6146c23a26bf7e8f52fbbe4
bulatziganshin-byte <bulatziganshin.work@gmail.com>
2026-09-07 18:40:30 +0300

    6.3 Разбираем Runner, Case и Config. Практическое задание: Создаём coursesRunner и подключаем его к тестам курсов

--- tests/axiom_courses_test.go
+++ tests/axiom_courses_test.go
@@ -12,34 +12,57 @@ type axiomCourse struct {
 	Published bool
 }
 
+var coursesRunner = testRunner.Join(
+	axiom.NewRunner(
+		axiom.WithRunnerMeta(
+			axiom.WithMetaTag("courses"),
+		),
+	),
+)
+
 func TestAxiomCreateCourse(t *testing.T) {
-	testRunner := axiom.NewRunner()
 	testCase := axiom.NewCase(
 		axiom.WithCaseName("course can be created"),
+		axiom.WithCaseMeta(
+			axiom.WithMetaTag("create"),
+		),
 	)
 
-	testRunner.RunCase(t, testCase, func(cfg *axiom.Config) {
+	coursesRunner.RunCase(t, testCase, func(cfg *axiom.Config) {
 		course := axiomCourse{
 			Title:     "Go API Autotests",
 			Published: false,
 		}
 
+		assert.Equal(
+			cfg.T(),
+			[]string{"axiom", "courses", "create"},
+			cfg.Meta.Tags,
+		)
 		assert.Equal(cfg.T(), "Go API Autotests", course.Title)
 		assert.False(cfg.T(), course.Published)
 	})
 }
 
-func TestAxiomPublishedCourse(t *testing.T) {
-	testRunner := axiom.NewRunner()
+func TestAxiomPublishCourse(t *testing.T) {
 	testCase := axiom.NewCase(
 		axiom.WithCaseName("course can be published"),
+		axiom.WithCaseMeta(
+			axiom.WithMetaTag("publish"),
+		),
 	)
 
-	testRunner.RunCase(t, testCase, func(cfg *axiom.Config) {
+	coursesRunner.RunCase(t, testCase, func(cfg *axiom.Config) {
 		course := axiomCourse{
 			Title:     "Go API Autotests",
 			Published: false,
 		}
+
+		assert.Equal(
+			cfg.T(),
+			[]string{"axiom", "courses", "publish"},
+			cfg.Meta.Tags,
+		)
 
 		course.Published = true
 
