08d5db102359eef882ba0c75ecb3a1c5406e3d6c
bulatziganshin-byte <bulatziganshin.work@gmail.com>
2026-09-11 16:30:16 +0300

    7.2 Создаём генератор тестовых данных. Практическое задание: дополняем генератор тестовых данных

--- fake/fake.go
+++ fake/fake.go
@@ -41,3 +41,39 @@ func (f *Fake) LastName() string {
 func (f *Fake) MiddleName() string {
 	return f.faker.MiddleName()
 }
+
+func (f *Fake) Title() string {
+	return f.faker.Sentence(4)
+}
+
+func (f *Fake) Description() string {
+	return f.faker.Sentence(12)
+}
+
+func (f *Fake) Filename() string {
+	return fmt.Sprintf("file-%s.txt", f.faker.UUID())
+}
+
+func (f *Fake) Directory() string {
+	return fmt.Sprintf("uploads/%s", f.faker.UUID())
+}
+
+func (f *Fake) Content() []byte {
+	return []byte(f.faker.Sentence(20))
+}
+
+func (f *Fake) MaxScore() int {
+	return f.faker.Number(50, 100)
+}
+
+func (f *Fake) MinScore() int {
+	return f.faker.Number(1, 30)
+}
+
+func (f *Fake) EstimatedTime() string {
+	return fmt.Sprintf("%d weeks", f.faker.Number(1, 10))
+}
+
+func (f *Fake) OrderIndex() int {
+	return f.faker.Number(1, 100)
+}
