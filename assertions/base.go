79d91453e77129f1cd61e98401303f64c0a6838b
bulatziganshin-byte <bulatziganshin.work@gmail.com>
2026-09-19 14:27:51 +0300

    11.1 Создаём базовые Assertions. Практическое задание: добавляем базовые Assertions

--- assertions/base.go
+++ assertions/base.go
@@ -31,14 +31,32 @@ func (a *Assertion) Equal(actual, expected any, name string) {
 	})
 }
 
+func (a *Assertion) Nil(actual any, name string) {
+	a.cfg.Step(fmt.Sprintf("Check that %s is nil", name), func() {
+		a.require.Nil(actual, "Value must be nil: %s", name)
+	})
+}
+
 func (a *Assertion) NotNil(actual any, name string) {
 	a.cfg.Step(fmt.Sprintf("Check that %s is not nil", name), func() {
 		a.require.NotNil(actual, "Value must not be nil: %s", name)
 	})
 }
 
+func (a *Assertion) NotEmpty(actual any, name string) {
+	a.cfg.Step(fmt.Sprintf("Check that %s is not empty", name), func() {
+		a.require.NotEmpty(actual, "Value must not be empty: %s", name)
+	})
+}
+
 func (a *Assertion) Error(actual error, name string) {
 	a.cfg.Step(fmt.Sprintf("Check that %s contains an error", name), func() {
 		a.require.Error(actual, "Expected an error: %s", name)
+	})
+}
+
+func (a *Assertion) Fail(message string) {
+	a.cfg.Step(fmt.Sprint(message), func() {
+		a.require.Fail(message)
 	})
 }
