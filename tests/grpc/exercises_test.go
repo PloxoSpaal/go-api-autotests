ccfcd822a99554061c678a7a80e29b1bf64b2e46
bulatziganshin-byte <bulatziganshin.work@gmail.com>
2026-09-22 13:07:43 +0300

    14.6 Практикуемся в написании gRPC API-автотестов. Часть 3. Практическое задание: проверяем получение задания по ID

--- tests/grpc/exercises_test.go
+++ tests/grpc/exercises_test.go
@@ -1,6 +1,7 @@
 package grpc
 
 import (
+	v1 "github.com/Nikita-Filonov/api-go-autotests-server/gen/go/v1"
 	"github.com/Nikita-Filonov/axiom"
 	"github.com/PloxoSpaal/go-api-autotests/builders"
 	"github.com/PloxoSpaal/go-api-autotests/metadata"
@@ -26,5 +27,26 @@ func (s *suite) TestCreateExercise() {
 
 		tools.Assertion.NoError(err)
 		tools.Assertion.GRPCCreateExerciseResponse(response, create)
+	}))
+}
+
+func (s *suite) TestGetExercise() {
+	testCase := axiom.NewCase(
+		axiom.WithCaseID("GRPC-EXERCISES-002"),
+		axiom.WithCaseName("get exercise"),
+		axiom.WithCaseMeta(
+			axiom.WithMetaTag(metadata.TagSmoke),
+			axiom.WithMetaStory(metadata.StoryGetEntity),
+			axiom.WithMetaSeverity(axiom.SeverityCritical),
+		),
+	)
+
+	s.RunCase(testCase, suiteToolset.Action(func(cfg *axiom.Config, tools *suiteTools) {
+		exercise := tools.Exercise()
+		request := &v1.GetExerciseRequest{Id: exercise.Response.GetExercise().GetId()}
+		response, err := tools.ExercisesClient().Get(cfg.Context.Raw, request)
+
+		tools.Assertion.NoError(err)
+		tools.Assertion.GRPCGetExerciseResponse(response, exercise.Response)
 	}))
 }
